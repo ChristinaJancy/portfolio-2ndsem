22a09fd86145bd86bb14dba73a4ba276596c1847
ChristinaJancy <chri91eq@easv365.dk>
2020-03-26 14:05:11 +0100

    Update Navigation.vue

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -2,21 +2,23 @@
   <div>
     <v-navigation-drawer 
     v-model="drawer" 
-    style="border-radius: 0px 0px 200px 0px;"
+    style="border-radius: 0px 0px 200px 0px; "
     fixed
     dark
-    class="primary"
-    left
-    width="110"
-    height="170">
-      <v-list dense justify="left" align="left">
+    class="text"
+    floating
+    clipped
+    mobile-break-point="600px"
+    width="100"
+    height="200">
+      <v-list dense justify="left" align="left" style="padding-top:50px;">
         <!-- Home-->
         
         <router-link to="/">
           <v-list-item link>
             <v-list-item-content>
               <!--@click="$vuetify.goTo(home, options)"-->
-              <v-list-item-title class="subtitle-2">Home</v-list-item-title>
+              <v-list-item-title class="subtitle-2 primary--text">Home</v-list-item-title>
             </v-list-item-content>
           </v-list-item>
         </router-link>
@@ -26,7 +28,7 @@
           <v-list-item link>
             <v-list-item-content>
               <!--@click="$vuetify.goTo(aboutMe, options)" -->
-              <v-list-item-title class="subtitle-2">My work</v-list-item-title>
+              <v-list-item-title class="subtitle-2 primary--text">My work</v-list-item-title>
             </v-list-item-content>
           </v-list-item>
         </router-link>
@@ -34,9 +36,10 @@
         <!-- about  -->
         <router-link to="/about">
           <v-list-item link>
+            
             <v-list-item-content>
               <!--@click="$vuetify.goTo(skills, options)"-->
-              <v-list-item-title class="subtitle-2">About me</v-list-item-title>
+              <v-list-item-title class="subtitle-2 primary--text">About me</v-list-item-title>
             </v-list-item-content>
           </v-list-item>
         </router-link>
@@ -47,7 +50,7 @@
     <v-app-bar 
     app 
     dense
-    width="160"
+    width="70"
     class="primary"
     style="border-radius: 0px 0px 30px 0px;" 
   >
