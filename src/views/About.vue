3b0276acb465be68e78af7210139bafdbaf70ba3
ChristinaJancy <chri91eq@easv365.dk>
2020-04-12 21:36:45 +0200

    Edit home and about me

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,5 @@
 <template>
-  <v-container style="margin-top:-80px; padding-top:50px;" class="primary" fluid>
+  <div style="margin-top:-80px; padding-top:50px;" class="primary" fluid>
  <h1 role="img" class="textLine text--text" aria-label="All projects.">
   <span class="letter">A</span>
   <span class="letter rev">b</span>
@@ -10,5 +10,371 @@
   <span class="letter">e</span>
   <span class="letter">.</span>
 </h1>
-  </v-container>
+<div class="container">
+    <div class="box">
+        <div class="title">
+            <span class="block"></span>
+            <h1 class="secondary--text">Christina Jancy<span></span></h1>
+        </div>
+
+        <div class="role">
+            <div class="block"></div>
+            <p class="secondary--text">MMD student</p>
+        </div>
+    </div>
+</div>
+<br>
+
+<v-col cols="6">
+<div class="pt-10">
+<h4 class="wordCarousel pa-10">    
+      <span class="secondary--text"> with experience in</span>  
+        <div> 
+<!--        Use classes 2,3, or 4 to match the number of words --> 
+            <ul class="flip4"> 
+                <li class="button--text">Content creation</li>
+                <li class="button--text">Web design</li>
+                <li class="button--text">User Experience</li> 
+                <li class="button--text">Business</li>  
+            </ul>
+        </div>  
+    </h4>
+  </div>
+</v-col>
+<v-col cols="6">
+
+</v-col>
+  </div>
 </template>
+
+
+<style lang="scss" scoped>
+//I am __ designer text scss
+:root {
+  --primary-color: #6CD9CE;
+  --secondary-color: #D93BA1;
+  --complimentary-color: #2E2473;
+}
+
+.h1_intro {
+  color: var(--primary-color);
+  transform: translateY(-600px);
+  animation: 1.2s slideIn ease-in-out forwards 1s;
+  opacity: 0;
+  position: relative;
+}
+
+.h1_intro::before {
+    content: '';
+    position: absolute;
+    animation: 1s underline ease-in-out forwards 2s;
+    mix-blend-mode: screen;
+}
+
+@keyframes skewBg {
+  0% {
+    transform: scale(.5);
+  }
+  100% {
+    transform: scale(1);
+  }
+}
+
+@keyframes underline {
+  100% {
+    width: 100%;
+  }
+}
+
+@keyframes slideIn {
+  100% {
+    transform: translateY(0px);
+    opacity: 1;
+  }
+}
+/* 
+
+About me text 
+
+*/
+@import url('https://fonts.googleapis.com/css?family=Lato:300,400|Poppins:300,400,800&display=swap');
+
+
+.container { 
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+  display: flex;
+
+  .box {
+    width:auto;
+    height: auto;
+    position: relative;
+    justify-content: center;
+    flex-direction: column;
+
+    .title {
+      width: 100%;
+      position: relative;
+      display: flex;
+      align-items: center;
+      height: 50px;
+
+      .block {
+        width: 0%;
+        height: inherit;
+        background: #ffb510;
+        position: absolute;
+        animation: mainBlock 2s cubic-bezier(.74, .06, .4, .92) forwards;
+        display: flex;
+      }
+
+      h1 {
+        font: 200 5vw 'Crimson Pro', serif;
+        color: #fff;
+        -webkit-animation: mainFadeIn 2s forwards;
+        -o-animation: mainFadeIn 2s forwards;
+        animation: mainFadeIn 2s forwards;
+        animation-delay: 1.6s;
+        opacity: 0;
+        align-items: baseline;
+        position: relative;
+
+        span {
+          width:0px;
+          height: 0px;
+          -webkit-border-radius: 50%;
+          -moz-border-radius: 50%;
+          border-radius: 50%;
+          background: #ffb510;
+          -webkit-animation: load 0.6s cubic-bezier(.74, .06, .4, .92) forwards;
+          animation: popIn 0.8s cubic-bezier(.74, .06, .4, .92) forwards;
+          animation-delay: 2s;
+          margin-left: 5px;
+          margin-top: -10px;
+          position: absolute;
+          bottom: 13px;
+          right: -12px;
+
+        }
+      }
+    }
+
+    .role {
+      width: 100%;
+      position: relative;
+      display: flex;
+      align-items: center;
+      height: 30px;
+      margin-top: 0px;
+
+      .block {
+        width: 0%;
+        height: inherit;
+        background: #e91e63;
+        position: absolute;
+        animation: secBlock 2s cubic-bezier(.74, .06, .4, .92) forwards;
+        animation-delay: 2s;
+        display: flex;
+      }
+
+      p {
+        animation: secFadeIn 2s forwards;
+        animation-delay: 3.2s;
+        opacity: 0;
+        padding-top:30px;
+        text-transform: uppercase;
+        letter-spacing: 5px;
+        font: 200 2vw 'Crimson Pro', serif
+      }
+    }
+  }
+}
+
+
+@keyframes mainBlock {
+  0% {
+    width: 0%;
+    left: 0;
+
+  }
+  50% {
+    width: 100%;
+    left: 0;
+
+  }
+  100% {
+    width: 0;
+    left: 100%;
+  }
+}
+
+@keyframes secBlock {
+  0% {
+    width: 0%;
+    left: 0;
+
+  }
+  50% {
+    width: 100%;
+    left: 0;
+
+  }
+  100% {
+    width: 0;
+    left: 100%;
+  }
+}
+
+@keyframes mainFadeIn {
+  0% {
+    opacity: 0;
+  }
+  100% {
+    opacity: 1;
+  }
+}
+
+
+@keyframes popIn {
+  0% {
+    width: 0px;
+    height: 0px;
+    background: #e9d856;
+    border: 0px solid #ddd;
+    opacity: 0;
+  }
+  50% {
+    width: 10px;
+    height: 10px;
+    background: #e9d856;
+    opacity: 1;
+    bottom: 45px;
+  }
+   65% {
+      width: 7px;
+    height: 7px;
+      bottom: 0px;
+      width: 15px
+   }
+   80% {
+      width: 10px;
+    height: 10px;
+      bottom: 20px
+   }
+  100% {
+    width: 7px;
+    height: 7px;
+    background: #e9d856;
+    border: 0px solid #222;
+    bottom: 13px;
+
+  }
+}
+
+@keyframes secFadeIn {
+  0% {
+    opacity: 0;
+  }
+  100% {
+    opacity: 0.5;
+  }
+}
+
+
+@keyframes top {
+  0% {
+    opacity: 0;
+     bottom: -80px
+  }
+  100% {
+    opacity: 1;
+     bottom: 0px
+
+  }
+}
+
+@keyframes icon {
+  0% {
+    opacity: 0;
+     transform: scale(0.0);
+  }
+   50% {
+      opacity: 1;
+     transform: scale(1.3) rotate(-02deg);
+   }
+  100% {
+    opacity: 1;
+     bottom: 0px;
+  }
+}
+
+/* 
+
+Word Carousel 
+
+*/
+
+.wordCarousel {
+    font: 200 3vw 'Crimson Pro', serif;
+    color: #939393;
+    div {
+        overflow: hidden;
+        position: relative;
+        height: 65px;
+        padding-top: 10px;
+        margin-top: -10px;
+        li {
+            font: 200 3vw 'Crimson Pro', serif;
+            color: #ccc;
+            position: relative;
+            font-weight: 700;
+            height: 45px;
+            margin-bottom: 45px;
+            display: block;
+        }
+    }
+}
+
+.flip2 { animation: flip2 6s cubic-bezier(0.23, 1, 0.32, 1.2) infinite; }
+.flip3 { animation: flip3 8s cubic-bezier(0.23, 1, 0.32, 1.2) infinite; }
+.flip4 { animation: flip4 10s cubic-bezier(0.23, 1, 0.32, 1.2) infinite; }
+
+@keyframes flip2 {
+    0% { margin-top: -180px; }
+    5% { margin-top: -90px;  }
+    50% { margin-top: -90px; }
+    55% { margin-top: 0px; }
+    99.99% { margin-top: 0px; }
+    100% { margin-top: -270px; }
+}
+
+@keyframes flip3 {
+    0% { margin-top: -270px; }
+    5% { margin-top: -180px; }
+    33% { margin-top: -180px; }
+    38% { margin-top: -90px; }
+    66% { margin-top: -90px; }
+    71% { margin-top: 0px; }
+    99.99% { margin-top: 0px; }
+    100% { margin-top: -270px; }
+}
+
+@keyframes flip4 {
+    0% { margin-top: -360px; }
+    5% { margin-top: -270px; }
+    25% { margin-top: -270px; }
+    30% { margin-top: -180px; }
+    50% { margin-top: -180px; }
+    55% { margin-top: -90px; }
+    75% { margin-top: -90px; }
+    80% { margin-top: 0px; }
+    99.99% { margin-top: 0px; }
+    100% { margin-top: -270px; }
+}
+
+// Layout //////////////////////////////////////////
+
+@import url(https://fonts.googleapis.com/css?family=Roboto+Condensed:300|Oswald);
+
+</style>
