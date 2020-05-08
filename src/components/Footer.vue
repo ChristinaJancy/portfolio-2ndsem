f96fbcd48971cbd955b9ee8ce2c61699a4647c6f
ChristinaJancy <chri91eq@easv365.dk>
2020-05-08 10:29:21 +0200

    Update Footer.vue

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -12,52 +12,56 @@
         <a
           href="mailto:chri91eq@easv360.dk"
           class="primary--text font-weight-bold"
-        >chri91eq@easv360.dk</a> 
+        >chri91eq@easv360.dk</a>
       </p>
     </v-col>
 
     <v-footer dark padless>
       <v-card flat tile class="secondary flex text-center">
-        <br><br>
+        <br />
+        <br />
         <v-card-text>
-        <v-row>
-          <v-col md="3" sm="12">
-          <a href="https://www.facebook.com/christinajancy/" target="_blank">
-            <v-btn class="mx-6 primary--text" icon>
-              <v-icon size="60">mdi-facebook</v-icon>
-            </v-btn>
-          </a>
-          <br><br>
-          </v-col>
-          <v-col md="3" sm="12">
-          <a href="https://github.com/ChristinaJancy" target="_blank">
-            <v-btn dark class="mx-6 primary--text" icon>
-              <v-icon style="font-size:60px;" class="fa fa-github"></v-icon>
-            </v-btn>
-          </a>
-              <br><br>
-        </v-col>
-        <v-col md="3" sm="12">
-          <a href="https://www.linkedin.com/in/christinajancy/" target="_blank">
-            <v-btn dark class="mx-6 primary--text" icon>
-              <v-icon size="60">mdi-linkedin</v-icon>
-            </v-btn>
-          </a>
-              <br><br>
-          </v-col>
-          <v-col md="3" sm="12">
-          <a href="https://www.instagram.com/christinajancy/?hl=da" target="_blank">
-            <v-btn dark class="mx-6 primary--text" icon>
-              <v-icon size="60">mdi-instagram</v-icon>
-            </v-btn>
-          </a>
-          </v-col>
+          <v-row>
+            <v-col md="3" sm="12">
+              <a href="https://www.facebook.com/christinajancy/" target="_blank">
+                <v-btn class="mx-6 primary--text" icon>
+                  <v-icon size="60">mdi-facebook</v-icon>
+                </v-btn>
+              </a>
+              <br />
+              <br />
+            </v-col>
+            <v-col md="3" sm="12">
+              <a href="https://github.com/ChristinaJancy" target="_blank">
+                <v-btn dark class="mx-6 primary--text" icon>
+                  <v-icon style="font-size:60px;" class="fa fa-github"></v-icon>
+                </v-btn>
+              </a>
+              <br />
+              <br />
+            </v-col>
+            <v-col md="3" sm="12">
+              <a href="https://www.linkedin.com/in/christinajancy/" target="_blank">
+                <v-btn dark class="mx-6 primary--text" icon>
+                  <v-icon size="60">mdi-linkedin</v-icon>
+                </v-btn>
+              </a>
+              <br />
+              <br />
+            </v-col>
+            <v-col md="3" sm="12">
+              <a href="https://www.instagram.com/christinajancy/?hl=da" target="_blank">
+                <v-btn dark class="mx-6 primary--text" icon>
+                  <v-icon size="60">mdi-instagram</v-icon>
+                </v-btn>
+              </a>
+            </v-col>
           </v-row>
         </v-card-text>
 
- <v-card-text class="secondary--text pt-0">
-        Phasellus feugiat arcu sapien, et iaculis ipsum elementum sit amet. Mauris cursus commodo interdum. Praesent ut risus eget metus luctus accumsan id ultrices nunc. Sed at orci sed massa consectetur dignissim a sit amet dui. Duis commodo vitae velit et faucibus. Morbi vehicula lacinia malesuada. Nulla placerat augue vel ipsum ultrices, cursus iaculis dui sollicitudin. Vestibulum eu ipsum vel diam elementum tempor vel ut orci. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.
-      </v-card-text>
+        <v-card-text
+          class="secondary--text pt-0"
+        >Phasellus feugiat arcu sapien, et iaculis ipsum elementum sit amet. Mauris cursus commodo interdum. Praesent ut risus eget metus luctus accumsan id ultrices nunc. Sed at orci sed massa consectetur dignissim a sit amet dui. Duis commodo vitae velit et faucibus. Morbi vehicula lacinia malesuada. Nulla placerat augue vel ipsum ultrices, cursus iaculis dui sollicitudin. Vestibulum eu ipsum vel diam elementum tempor vel ut orci. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.</v-card-text>
         <v-divider></v-divider>
 
         <v-card-text class="primary--text">
